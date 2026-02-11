84503b71c998457c12079fcb8323dd92e8996867
dubontemps <atimsit@gmail.com>
2026-02-11 13:32:51 +0100

    v80 contact correc

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import React, { useState, useEffect, useMemo, useRef } from 'react';
 import { motion, useScroll, AnimatePresence } from 'framer-motion';
 import { X } from 'lucide-react';
 import './App.css';
